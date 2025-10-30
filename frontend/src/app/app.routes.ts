462d34c5decdb6e70428ef03ff4a694b66912165
RKCbas <sebashp2015@hotmail.com>
2025-10-30 08:08:07 -0600

    route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,4 +5,16 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import("./landing/landing.routes")
   },
+  {
+    path: 'products',
+    loadChildren: () => import("./products/products.routes")
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import("./auth/auth.routes")
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
