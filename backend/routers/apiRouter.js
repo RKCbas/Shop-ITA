d11c6b4f81cb7435a2ef708075eb511d7d5da355
RKCbas <sebashp2015@hotmail.com>
2025-10-29 19:09:44 -0600

    Basic Configuration

--- backend/routers/apiRouter.js
+++ backend/routers/apiRouter.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const apiRouter = Router();
 
-// apiRouter.use('/ruta', router);
+apiRouter.use('/', (_req, res) => {
+    res.status(200).send('Ping')
+});
 
 export default apiRouter;
