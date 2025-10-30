bf707ef858442ea5a9c3ef5c8e10797223182548
RKCbas <sebashp2015@hotmail.com>
2025-10-30 08:51:41 -0600

    Basic config

--- frontend/src/app/products/components/navbar/navbar.ts
+++ frontend/src/app/products/components/navbar/navbar.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
 })
-export class Navbar { }
+export class Navbar {
+
+  searchQuery = signal<string>('')
+
+  router = inject(Router)
+
+  onSearch() {
+    const value = this.searchQuery().trim();
+
+    if (!value) return;
+
+    this.router.navigate(['/products/search'], {
+      queryParams: { query: this.searchQuery() },
+    });
+  }
+
+}
